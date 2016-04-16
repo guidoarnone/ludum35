d3727802cef9d339bb6b0a59d5a019eaedd3b57a
Eric Brandwein <brandweineric@gmail.com>
2016-04-16 15:01:00 -0300

    Added character assets, added pickupable objects

---
+++ Assets/Pickupper.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickupper : MonoBehaviour {
+
+	private Camera cam;
+	private bool carrying;
+	private GameObject carried;
+
+	public float smooth;
+	public float distance;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (carrying) {
+			carry ();
+		} else {
+			pickup ();
+		}
+	}
+
+	void carry(){
+		Vector3 actualPos = cam.transform.position + cam.transform.forward * distance;
+		carried.transform.position = Vector3.Lerp (carried.transform.position, actualPos, Time.deltaTime * smooth);
+	}
+
+	void pickup(){
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastHit hit;
+			if (Physics.Raycast (cam.transform.position, cam.transform.forward, out hit)) {
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if (p != null) {
+					carrying = true;
+					carried = p.gameObject;
+				}
+			}
+		}
+	}
+}
