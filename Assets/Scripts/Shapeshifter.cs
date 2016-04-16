760f5ca7d58b41cc65b2b69628d2691bc4ae5c98
guidoarnone <guido.arnone@mercadolibre.com>
2016-04-16 16:26:18 -0300

    shapeshifting (sway still not working)

---
+++ Assets/Scripts/Shapeshifter.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shapeshifter : MonoBehaviour {
+
+	private SkinnedMeshRenderer skinnedMeshRenderer;
+	private Mesh skinnedMesh;
+	private int blendShapeCount;
+
+	private List<Transition> transitionList;
+	private bool onTransition = false;
+
+	void Start () {
+		this.skinnedMeshRenderer = gameObject.GetComponent<SkinnedMeshRenderer> ();
+		this.skinnedMesh = skinnedMeshRenderer.sharedMesh;
+		this.blendShapeCount = skinnedMesh.blendShapeCount;
+		if (this.blendShapeCount == 0) {
+			this.enabled = false;
+		}
+		this.transitionList = new List<Transition>();
+	}
+	
+
+	void Update () {
+		if (this.onTransition) {
+			this.updateTransitions();
+			this.checkTransitionsAndFinish();
+		}
+	}
+
+	public void startTransition(Transition transition) {
+		if(!this.onTransition) {
+			this.onTransition = true;
+			this.clearTransitions();
+			this.transitionList.Add(transition);
+		}
+	}
+
+	public void startTransition(List<Transition> transitions) {
+		if(!this.onTransition) {
+			this.onTransition = true;
+			this.clearTransitions();
+			for (int i = 0; i < transitions.Count; i++) {
+				this.transitionList.Add(transitions [i]);
+			}
+		}
+	}
+
+	public void setShapeshift(int index, float s) {
+		this.skinnedMeshRenderer.SetBlendShapeWeight (index, s);
+	}
+
+	public void setShapeshift(List<float> values) {
+		for(int v = 0; v < values.Count; v++) {
+			this.skinnedMeshRenderer.SetBlendShapeWeight (v, values [v]);
+		}
+	}
+
+	private void createTransitionList() {
+		for(int i = 0; i < this.blendShapeCount; i++) {
+			transitionList.Add (new Transition ());
+		}
+	}
+
+	private void clearTransitions() {
+		this.transitionList = new List<Transition> ();
+	}
+
+	private void checkTransitionsAndFinish() {
+		bool finished = true;
+		foreach (Transition t in this.transitionList) {
+			if (t.getCurrentValue () != t.getFinalValue ()) {
+				finished = false;
+			}
+		}
+		if (finished) {
+			this.onTransition = false;
+			this.clearTransitions ();
+		}
+	}
+
+	private void updateTransitions() {
+		for (int t = 0; t < transitionList.Count; t++) {
+			float nextValue = transitionList [t].getCurrentValue () + transitionList [t].getRate () * Time.deltaTime;
+			nextValue = Mathf.Min(nextValue, transitionList[t].getFinalValue());
+			this.setShapeshift (t, nextValue);
+			transitionList [t].setCurrentValue (nextValue);
+		}	
+	}
+
+	public Transition getDummyTransition(int blendShapeIndex) {
+		float defaultValue = this.skinnedMeshRenderer.GetBlendShapeWeight (blendShapeIndex);
+		return new Transition (defaultValue,0f,defaultValue);
+	}
+		
+	public Transition makeTransitionFromBlendShape(int blendShapeIndex, float rate, float finalValue) {
+		float initialValue = this.skinnedMeshRenderer.GetBlendShapeWeight (blendShapeIndex);
+		return new Transition (initialValue, rate, finalValue);
+	}
+
+	public List<Transition> makeTransitionsFromCurrentState(List<float> rates, List<float> finalValues) {
+		List<Transition> transitions = new List<Transition> ();
+		for(int i = 0; i < this.blendShapeCount; i++) {
+			transitions.Add (this.makeTransitionFromBlendShape(i, rates[i], finalValues[i]));
+		}
+		return transitions;
+	}
+
+	public bool isOnTransition() {
+		return this.onTransition;
+	}
+
+}
---
+++ Assets/Scripts/Sway.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Sway : MonoBehaviour {
+
+	public List<float> rates;
+	public List<float> initialValues;
+	public List<float> finalValues;
+
+	private Shapeshifter shapeshifter;
+	private int iterator = 0;
+
+	void Start () {
+		shapeshifter = this.GetComponent<Shapeshifter> ();
+		shapeshifter.setShapeshift (this.initialValues);
+		shapeshifter.startTransition (shapeshifter.makeTransitionsFromCurrentState (this.rates, this.finalValues));
+	}
+	
+	void Update () {
+		if (!shapeshifter.isOnTransition()) {
+			this.invert ();
+			Debug.Log (this.finalValues[0]);
+			shapeshifter.setShapeshift (this.initialValues);
+			shapeshifter.startTransition (shapeshifter.makeTransitionsFromCurrentState (this.rates, this.finalValues));
+		}
+	}
+
+	public void invert() {
+		for (int i = 0; i < rates.Count; i++) {
+			rates [i] *= -1;
+		}
+		List<float> auxiliar = initialValues;
+		this.initialValues = this.finalValues;
+		this.finalValues = auxiliar;
+	}
+	
+}
