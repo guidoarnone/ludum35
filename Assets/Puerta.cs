0106cfe3bb0539c4b4bba70a76eaa5217cf66c6b
Eric Brandwein <brandweineric@gmail.com>
2016-04-17 00:39:08 -0300

    Added animation to the door

--- Assets/Puerta.cs
+++ Assets/Puerta.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class Puerta : Button {
-	
+
+	int rotation=0;
+
+	public int speed=10;
+	public bool opened=false;
+
 	public override void pressButton () {
 		GameObject player = GameObject.FindGameObjectWithTag ("Player");
 		Inventory inventory = player.GetComponent<Inventory> ();
 		if (inventory.getHasKey ()) {
-			transform.RotateAround (transform.up, transform.up, 180);
+			opened = true;
 			inventory.setHasKey (false);
 		}
 	}
 
+	void Update(){
+		if (rotation < 90 && opened) {
+			transform.RotateAround (transform.right + transform.position, transform.up, speed);
+			rotation += speed;
+		}
+	}
 }
